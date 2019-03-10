352ae28ff5656f44976ab08ce2546e81852e68d1
JavineKuang <kuangyu1987@gmail.com>
2019-03-11 00:06:16 +0800

    添加弹出框

--- app/src/main/java/com/k/javine/warehousemanage/StockActivity.java
+++ app/src/main/java/com/k/javine/warehousemanage/StockActivity.java
@@ -7,7 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.k.javine.warehousemanage.adapter.BaseRecyclerAdapter;
@@ -23,12 +29,15 @@ public class StockActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private StockAdapter mAdapter;
+    private PopupWindow mPopupWindow;
+    private View mAnchor;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock);
         setTitle("库存");
         mRecyclerView = findViewById(R.id.list);
+        mAnchor = findViewById(R.id.anchor);
         mAdapter = new StockAdapter(this);
         mAdapter.setOnItemClickListener(new BaseRecyclerAdapter.OnItemClickListener<StockViewHolder>() {
             @Override
@@ -42,7 +51,50 @@ public class StockActivity extends AppCompatActivity {
         loadData();
     }
 
-    private void showDetailPage(StockItem itemByPosition) {
+    private View mContainerView;
+    private View mCloseView;
+    private TextView mTitleView;
+    private TextView mTotleView, mSizeView, mColorView;
+    private Button mBtnEnter, mBtnOutput;
+
+    private void showDetailPage(StockItem item) {
+        if (mPopupWindow == null) {
+            mPopupWindow = new PopupWindow(this);
+            mPopupWindow.setOutsideTouchable(false);
+            mPopupWindow.setAnimationStyle(R.style.PopupWindowAnimation);
+            mPopupWindow.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+            mPopupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+        updateWindowContent(item);
+        mPopupWindow.showAtLocation(findViewById(R.id.rootView), Gravity.BOTTOM, 0, 0);
+    }
+
+
+
+    private void updateWindowContent(StockItem item) {
+        if (mContainerView == null) {
+            mContainerView = LayoutInflater.from(this).inflate(R.layout.popup_window_layout, null);
+            mPopupWindow.setContentView(mContainerView);
+            mCloseView = mContainerView.findViewById(R.id.iv_close);
+            mTitleView = mContainerView.findViewById(R.id.tv_title);
+            mBtnEnter = mContainerView.findViewById(R.id.btn_input);
+            mBtnOutput = mContainerView.findViewById(R.id.btn_output);
+            mTotleView = mContainerView.findViewById(R.id.tv_totle);
+            mColorView = mContainerView.findViewById(R.id.tv_color);
+            mSizeView = mContainerView.findViewById(R.id.tv_size);
+
+            mCloseView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mPopupWindow.dismiss();
+                }
+            });
+        }
+        String title = item.getName() + " - " + item.getId();
+        mTitleView.setText(title);
+        mTotleView.setText(String.valueOf(item.getTotalCount()));
+        mColorView.setText(item.getColorString());
+        mSizeView.setText(item.getSizeString());
 
     }
 
--- app/src/main/java/com/k/javine/warehousemanage/data/StockItem.java
+++ app/src/main/java/com/k/javine/warehousemanage/data/StockItem.java
@@ -1,13 +1,23 @@
 package com.k.javine.warehousemanage.data;
 
+import android.content.Intent;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * @文件描述 : 库存列表item
  * @文件作者 : KuangYu
  * @创建时间 : 19-1-22
  */
 public class StockItem extends Product{
+    private HashMap<String, Integer> colorMap;
+    private HashMap<String, Integer> sizeMap;
     private int totalCount;
     private float totalMoney;
+    private String sizeString;
+    private String colorString;
 
     public int getTotalCount() {
         return totalCount;
@@ -23,5 +33,67 @@ public class StockItem extends Product{
 
     public void setTotalMoney(float totalMoney) {
         this.totalMoney = totalMoney;
+    }
+
+    public HashMap<String, Integer> getColorMap() {
+        return colorMap;
+    }
+
+    public void setColorMap(HashMap<String, Integer> colorMap) {
+        this.colorMap = colorMap;
+    }
+
+    public HashMap<String, Integer> getSizeMap() {
+        return sizeMap;
+    }
+
+    public void setSizeMap(HashMap<String, Integer> sizeMap) {
+        this.sizeMap = sizeMap;
+    }
+
+    public void changeSizeItem(String size, Integer value) {
+        if (sizeMap == null) {
+            sizeMap = new HashMap<>();
+        }
+        sizeMap.put(size, value);
+    }
+
+    public void changeColorItem(String key, Integer value) {
+        if (colorMap == null) {
+            colorMap = new HashMap<>();
+        }
+        colorMap.put(key, value);
+    }
+
+    public String getSizeString() {
+        if (sizeString == null) {
+            StringBuilder sizeBuilder = new StringBuilder();
+            for (Map.Entry<String, Integer> entry : sizeMap.entrySet()) {
+                if (entry.getValue() > 0) {
+                    sizeBuilder.append(entry.getKey())
+                            .append(" :  ")
+                            .append(entry.getValue())
+                            .append("\n");
+                }
+            }
+            sizeString = sizeBuilder.toString();
+        }
+        return sizeString;
+    }
+
+    public String getColorString() {
+        if (colorString == null) {
+            StringBuilder sizeBuilder = new StringBuilder();
+            for (Map.Entry<String, Integer> entry : colorMap.entrySet()) {
+                if (entry.getValue() > 0) {
+                    sizeBuilder.append(entry.getKey())
+                            .append(" :  ")
+                            .append(entry.getValue())
+                            .append("\n");
+                }
+            }
+            colorString = sizeBuilder.toString();
+        }
+        return colorString;
     }
 }
